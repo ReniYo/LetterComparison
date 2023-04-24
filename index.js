abe5d520853a76812e3fa59f01b4201af552859c
ReniYo <benoko@abv.bg>
2023-04-24 21:51:00 +0300

    updates on index.js and index.css
    code optimization

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
-// javascript
-
 const previousWord = document.getElementById("previous-word")
 const newWord = document.getElementById("new-word")
 const checkBtn = document.getElementById("check-btn")
 const resultEl = document.getElementById("result")
-let sameLettersCount = 0
-let crossedLettersCount = 0
-let sameLetters = ""
-let crossedLetters = ""
+let sameLettersArr = []
+let crossedLettersArr = []
 let previousWordArr = []
 let newWordArr = []
 
@@ -22,50 +18,30 @@ function handleCheckClick(){
         for(let i = 0; i < newWord.value.length; i++) {
             newWordArr.push(newWord.value[i])
         }
-        
-        for(let i = previousWordArr.length -1; i >= 0 ; i--) {
-            for(let j = 0; j < newWordArr.length; j++){
-                if(previousWordArr[i] === newWordArr[j]){
-                    sameLettersCount++
-                    sameLetters += `${previousWordArr[i]} `
-                    previousWordArr.splice(i, 1)
-                    newWordArr.splice(j, 1)
-                }
-            }
-        }
-        
-        crossedLettersCount = (previousWord.value.length + newWord.value.length) - sameLettersCount*2
-        for(letter of previousWordArr) {
-            crossedLetters += `${letter} `
-        }
-        
-        for(letter of newWordArr) {
-            crossedLetters += `${letter} `
-        }
-      
+
+        sameLettersArr = previousWordArr.filter((letter) => newWordArr.includes(letter))
+        crossedLettersArr = previousWordArr.filter((letter) => !newWordArr.includes(letter))
+    
         resultEl.innerHTML = `
                     <div class="words">
                         <p class="words-style">${previousWord.value}</p>
                         <p class="words-style">${newWord.value}</p>
                     </div>
-                        <p class="same-letters">Same letters: ${sameLettersCount} (${sameLetters})</p>
-                        <p class="crossed-letters">Crossed letters: ${crossedLettersCount} (${crossedLetters})</p>
+                        <p class="same-letters">Same letters: ${sameLettersArr.length} (${sameLettersArr})</p>
+                        <p class="crossed-letters">Crossed letters: ${crossedLettersArr.length} (${crossedLettersArr})</p>
         `
         
         previousWord.value = newWord.value
         previousWord.disabled = true
         newWord.value = ""
-        sameLettersCount = 0
-        crossedLettersCount = 0
-        sameLetters = ""
-        crossedLetters = ""
+        sameLettersArr = []
+        crossedLettersArr = []
         previousWordArr = []
         newWordArr = []
     }
     else{
         resultEl.innerHTML = `<p>Oh, Lord of Words, please, add the two words!</p>`
-    }
-    
+    } 
 }
 
 
